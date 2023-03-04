9fcb061cbd78e66fe56941c063b737078e35c335
Nick Kovalsky <nickyboom@mail.ru>
2023-03-04 11:05:01 +0300

    Windows basic support

---
+++ lib/Platforms/Windows/BluetoothPermissions.Windows.cs
@@ -0,0 +1,22 @@
+﻿namespace AppoMobi.Maui.BLE.Connector;
+
+
+public partial class BluetoothPermissions : Permissions.BasePlatformPermission
+{
+	private readonly bool _scan;
+	private readonly bool _advertise;
+	private readonly bool _connect;
+	private readonly bool _bluetoothLocation;
+
+
+
+
+	public static bool CheckGpsIsAvailable()
+	{
+		bool value = false;
+
+		return value;
+	}
+
+
+}
